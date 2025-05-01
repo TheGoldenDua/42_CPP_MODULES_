36cb52d9e7c46ed3b97f78386e91262292f1c42b
del-ganb <del-ganb@e2r10p12.1337.ma>
2025-05-01 15:43:32 +0100

    ex02 done

---
+++ 42_CPP01/ex02/main.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str("HI THIS IS BRAIN");
+    std::string &stringREF = str;
+    std::string *stringPTR = &str;
+
+    std::cout << "\n******************ADDRESSES**********************\n" << std::endl;
+
+    std::cout << "Memmory Address of the string: " << &str 
+              << "\nMemory Address of the string pointer: " << stringPTR
+              << "\nMemory Address of the string reference: " << &stringREF << std::endl;
+
+    std::cout << "\n******************VALUES**********************\n" << std::endl;
+
+    std::cout << "Value of the string: " << str 
+    << "\nValue of the string pointer: " << *stringPTR
+    << "\nValue of the string reference: " << stringREF << std::endl;
+
+    return 0;
+}
