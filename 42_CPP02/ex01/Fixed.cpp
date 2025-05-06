999eec5d564836b97889f87110fc973f70eb22a5
thegoldendua <douaeelganbour@gmail.com>
2025-05-06 17:09:53 +0100

    ex01 done

--- 42_CPP02/ex01/Fixed.cpp
+++ 42_CPP02/ex01/Fixed.cpp
@@ -5,6 +5,18 @@ Fixed::Fixed() : fPointval(0)
     std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const float f)
+{
+    std::cout << "Float constructor called" << std::endl;
+    fPointval = roundf(f * (1 << fractbit));
+}
+
+Fixed::Fixed(const int i)
+{
+    std::cout << "Int constructor called" << std::endl;
+    fPointval = i << fractbit;
+}
+
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
@@ -36,10 +48,16 @@ void Fixed::setRawbits(int const raw)
 
 int Fixed::toInt(void) const
 {
-
+    return (int) fPointval >> fractbit ;
 }
 
 float Fixed::toFloat(void) const
 {
+    return (float) fPointval / (1 << fractbit);
+}
 
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+    out << fixed.toFloat();
+    return out;
 }
--- 42_CPP02/ex01/Fixed.hpp
+++ 42_CPP02/ex01/Fixed.hpp
@@ -24,4 +24,6 @@ class Fixed
         int toInt( void ) const;
 };
 
+std::ostream &operator<<(std::ostream &out, const Fixed &Fixed);
+
 #endif
