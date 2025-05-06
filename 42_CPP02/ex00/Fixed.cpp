62d7c515a2241a38fedc7bdf3e91f980a9667181
thegoldendua <douaeelganbour@gmail.com>
2025-05-06 23:47:53 +0100

    done

--- 42_CPP02/ex00/Fixed.cpp
+++ 42_CPP02/ex00/Fixed.cpp
@@ -6,17 +6,17 @@ Fixed::Fixed() : value(0)
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other)
+Fixed::Fixed(const Fixed &copy)
 {
     std::cout << "Copy constructor called" << std::endl;
-    *this = other;
+    *this = copy;
 }
 
-Fixed&  Fixed::operator=(const Fixed& other)
+Fixed&  Fixed::operator=(const Fixed& copy)
 {
     std::cout << "Copy assignment operator called" << std::endl;
-    if(this != &other)
-        this->value = other.getRawBits();
+    if(this != &copy)
+        this->value = copy.getRawBits();
     return *this;
 }
 
