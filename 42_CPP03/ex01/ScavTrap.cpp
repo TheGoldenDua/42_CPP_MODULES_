3519f6002f78e20c62c84c66a4242f5db802f615
thegoldendua <douaeelganbour@gmail.com>
2025-05-08 20:31:54 +0100

    replace private with protected

--- 42_CPP03/ex01/ScavTrap.cpp
+++ 42_CPP03/ex01/ScavTrap.cpp
@@ -1,3 +1,9 @@
 #include "ScavTrap.hpp"
+#include "ClavTrap.hpp"
 #include <iostream>
+
+ScavTrap::ScavTrap() : ClavTrap("DefaultName") : _hitPoints(100), _energyPoints(50), _attackDamage(20) 
+{
+    std::cout << "ScavTrap default constructor called" << std::endl;
+}
 
