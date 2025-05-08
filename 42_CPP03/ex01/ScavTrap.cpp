3d01f19e37983d9650e891f2ae5e542eb0bf755e
del-ganb <del-ganb@e2r6p12.1337.ma>
2025-05-08 21:58:55 +0100

    ex01 done

--- 42_CPP03/ex01/ScavTrap.cpp
+++ 42_CPP03/ex01/ScavTrap.cpp
@@ -4,7 +4,7 @@
 
 ScavTrap::ScavTrap() : ClapTrap("DefaultName") 
 {
-    std::cout << "ScavTrap default constructor called" << std::endl;
+    std::cout << "ScavTrap default constructor called." << std::endl;
     this->_hitPoints = 100;
     this->_energyPoints = 50;
     this->_attackDamage = 20;
@@ -12,22 +12,45 @@ ScavTrap::ScavTrap() : ClapTrap("DefaultName")
 
 ScavTrap::ScavTrap(std::string name)
 {
-    std::cout << "ScavTrap parameterized constructor called" << std::endl;
+    std::cout << "ScavTrap parameterized constructor called." << std::endl;
     this->_name = name;
     this->_hitPoints = 100;
     this->_energyPoints = 50;
     this->_attackDamage = 20;
 }
+ScavTrap::ScavTrap(const ScavTrap& other):ClapTrap(other)
+{
+    std::cout << "ScavTrap Copy Constructor called." << std::endl;
+}
 
 ScavTrap& ScavTrap::operator=(const ScavTrap& other)
 {
-    std::cout << "ScavTrap copy constructor called." << std::endl;
-    *this = other;
+    std::cout << "ScavTrap Assignation operator called." << std::endl;
+    this->_name = other._name;
+    this->_hitPoints = other._hitPoints;
+    this->_energyPoints = other._energyPoints;
+    this->_attackDamage = other._attackDamage;
+    return *this;
 }
 
 ScavTrap::~ScavTrap()
 {
     std::cout << "ScavTrap destructor called." << std::endl;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+    if(this->_hitPoints > 0 && this->_energyPoints >= 1)
+    {
+        std::cout << "ScavTrap " << this->_name << " attacks " << target << ", causing " 
+                  << this->_attackDamage << " points of damage!" << std::endl;
+        this->_energyPoints--;        
+    }
+    else
+    {
+        std::cout << "ScavTrap " << this->_name << " can't attack " << target 
+                  << " due to insufficient hit points or energy." << std::endl;
+    }
 }
 
 void ScavTrap::guardGate(void) const
