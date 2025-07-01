98b11b3f310061784fe5835161f4d71e06cc7edc
del-ganb <del-ganb@e2r8p12.1337.ma>
2025-07-01 12:40:56 +0100

    have to implement the conversion of the type input and display it all

--- 42_CPP06/ex00/ScalarConverter.cpp
+++ 42_CPP06/ex00/ScalarConverter.cpp
@@ -1,22 +1,68 @@
 #include "ScalarConverter.hpp"
 
-static  std::string which_type(std::string const m_type)
+bool ScalarConverter::check_input(std::string const& str)
 {
-    if(m_type == "int")
-        return("int");
-    else if(m_type == "char")
-        return("char");
-    else if(m_type == "float")
-        return("float");
-    else if(m_type == "double")
-        return ("double");
-    else
-        return(NULL);
+    int i = 0;
+    if(str.empty())
+    {
+        std::cout << "Empty input!" << std::endl;
+        return (false);
+    }
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        if(!isprint(str[i]))
+        {
+            std::cout << "Non printable charachters!" << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
 
-void ScalarConverter::convert(std::string const& m_type)
+std::string ScalarConverter::which_type(std::string const& input)
 {
+    if(input == "nanf" || input ==  "-inff" || input == "+inff")
+        return "float";
+
+    if(input == "nan" || input == "-inf" || input == "+inf")
+        return "double"; 
+
+    if(input.length() == 1 && !std::isdigit(input[0]))
+        return "char";
+        
+    char *intEnd;
+    long val;
+    val = std::strtol(input.c_str(), &intEnd, 10);
+    if(*intEnd == '\0' && val >= INT_MIN && val <= INT_MAX)
+        return "int";
+
+    if(input.back() == 'f')
+    {
+        std::string fltPart;
+        char *floatEnd;
+
+        fltPart = input.substr(0, input.length() - 1);
+        std::strtod(fltPart.c_str(), &floatEnd);
+        if(*floatEnd == '\0')
+            return "float";
+    }
+
+    char *doublEnd;
+    std::strtod(input.c_str(), &doublEnd);
+    if(*doublEnd == '\0')
+        return "double";
+
+    return "unknown";
+}
+
+void ScalarConverter::convert(std::string const& input)
+{
+    if (!check_input(input))
+        return;
+
     std::string type;
-
-    type = which_type(m_type);
+    type = which_type(input);
+    std::cout << "Detected type: " << type << std::endl;
 }
+
+
--- 42_CPP06/ex00/ScalarConverter.hpp
+++ 42_CPP06/ex00/ScalarConverter.hpp
@@ -2,6 +2,9 @@
 #define SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <cmath>
+#include <cctype>
+#include <climits>
 
 class  ScalarConverter
 {
@@ -11,8 +14,11 @@ class  ScalarConverter
         ScalarConverter&  operator=(const ScalarConverter& other);
         ~ScalarConverter();
 
+        static std::string which_type(std::string const& m_type);
+        static bool check_input(std::string const &str); 
+
     public:
-        static void convert(std::string const &m_type);
+        static void convert(std::string const& m_type);
 };
 
 #endif
