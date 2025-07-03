c894485cd4a21d6824b8440f12539e96eaabf35d
del-ganb <del-ganb@e2r6p12.1337.ma>
2025-07-03 12:25:57 +0100

    done

--- 42_CPP06/ex01/Data.hpp
+++ 42_CPP06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: del-ganb <del-ganb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 12:12:21 by del-ganb          #+#    #+#             */
-/*   Updated: 2025/07/03 12:12:22 by del-ganb         ###   ########.fr       */
+/*   Updated: 2025/07/03 12:24:13 by del-ganb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 struct Data
 {
     int i;
-    char *str;
+    std::string str;
     char c;
 };
 
