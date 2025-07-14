4adbd57e01d9ed0be75ebc1ae63be2c67ae9233c
del-ganb <del-ganb@c4r9p5.1337.ma>
2025-07-14 12:07:05 +0100

    add an array of chars

--- 42_CPP07/ex01/main.cpp
+++ 42_CPP07/ex01/main.cpp
@@ -2,12 +2,16 @@
 
 int main()
 {
-    std::cout << "printing an array of integers" << std::endl;
-    int T[5] = {1, 2, 3, 4, 5};
-    iter(T, 5, print);
+    std::cout << "printing an array of integers:" << std::endl;
+    int I[5] = {1, 2, 3, 4, 5};
+    iter(I, 5, print);
 
-    std::cout << "\nprinting an array of strings" << std::endl;
+    std::cout << "\nprinting an array of strings:" << std::endl;
     std::string S[] = {"hello", "world", "I am", "using", "templates!"};
     iter(S, 5, print);
+
+    std::cout << "\nprinting an array of characters:" << std::endl;
+    char C[] = {'h', 'e', 'l', 'l', 'o'};
+    iter(C, 5, print);
     return 0;
 }
