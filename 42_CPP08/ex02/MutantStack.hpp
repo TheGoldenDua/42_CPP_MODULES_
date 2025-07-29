5d848de214d33b868eb11d4f0f1d222ee6619f12
del-ganb <del-ganb@c4r1p2.1337.ma>
2025-07-29 15:26:57 +0100

    starting ex02

---
+++ 42_CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,14 @@
+#ifndef MUSTANTSTACK_HPP
+#define MUSTANTSTACK_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+
+};
+
+#endif
